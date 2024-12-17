699bc88fe440450a402dec7921ca6850d7ed783f
4ra1n <2023503307@qq.com>
2024-12-17 22:47:57 +0800

    after release

--- src/main/test/me/n1ar4/test/TestAPI.java
+++ src/main/test/me/n1ar4/test/TestAPI.java
@@ -8,30 +8,7 @@ import java.util.Base64;
 
 public class TestAPI {
     public static void main(String[] args) {
-        BaseConfig config = new BaseConfig();
-        config.setLogLevel("info");
-        // 默认不开隐藏
-        config.setEnableHideField(false);
-        config.setEnableHideMethod(false);
-
-        // 默认开启所有混淆
-        config.setEnableAdvanceString(true);
-        config.setEnableFieldName(true);
-        config.setEnableXOR(true);
-        config.setEnableDeleteCompileInfo(true);
-        config.setEnableParamName(true);
-        config.setEnableMethodName(true);
-
-        // 默认花指令配置
-        config.setEnableJunk(true);
-        config.setJunkLevel(3);
-        config.setMaxJunkOneClass(1000);
-
-        // 默认额外配置
-        config.setObfuscateChars(new String[]{"i", "l", "L", "1", "I"});
-        config.setAdvanceStringName("iii");
-        config.setMethodBlackList(new String[]{"main"});
-
+        BaseConfig config = BaseConfig.Default();
         ClassObf classObf = new ClassObf(config);
         Result result = classObf.run("target/test-classes/me/n1ar4/test/TestAPI.class");
         if (result.getMessage().equals(Result.SUCCESS)) {
